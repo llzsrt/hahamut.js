233a8d3c1e93d2184b5500a4058599a741a7c103
llzsrt <pencil0325@gmail.com>
2019-02-06 00:05:32 +0800

    message filter

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { HahamutBot } from './HahamutBot';
 export { HahamutMessage } from './HahamutMessage';
 export { Message } from './types/Message';
+export { MessageFilter } from './MessageFilter';
+export { FilterMethod } from './emun/FilterMethod';
