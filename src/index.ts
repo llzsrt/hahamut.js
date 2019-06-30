a31e3f0f010b073438c86cf7866f70070bd88a9b
llzsrt <pencil0325@gmail.com>
2019-06-30 12:02:35 +0800

    index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-export { HahamutBot } from './HahamutBot';
-export { ReceivedMessage } from './ReceivedMessage';
-export { MessageTrigger } from './MessageTrigger';
+export * from './HahamutBot';
+export * from './ReceivedMessage';
+export * from './MessageTrigger';
 export * from './enums';
 export * from './types/BotEvent';
 export * from './types/Message';
